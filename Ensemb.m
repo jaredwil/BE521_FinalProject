63053d9968ec3282a4c5cef71a692eca160a3988
jaredwil <jaredwil@seas.upenn.edu>
2015-04-18 14:06:08 -0400

    chanes add

--- Ensemb.m
+++ Ensemb.m
@@ -1,7 +1,8 @@
 function [ enW ] = Ensemb( model1, model2, ...however many we have )
 %Ensemb - this function will be used to combine all models that have been
 %made to create an ensemble prediction that will be super bad ass.
-%jfkdsla;fjdsa;
+%
+%test
 
 
 %WIN
